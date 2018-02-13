dca447921b83a811c56ec4c84c2b085ee20ab558
Marko <markgolovko@gmail.com>
2018-02-13 17:18:18 +0100

    Add function to adding books

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        public static List<string> books = new List<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String cki;
+            Console.WriteLine("Welcome to library");
+            do
+            {
+                cki = Console.ReadLine();
+                if (cki == "Add"){
+                    cki = Console.ReadLine();
+                    books = AddB(cki);
+                }
+                if (cki == "My books")
+                {
+                    foreach (var item in books){
+                        Console.WriteLine(item);
+                    }
+                }
+
+            } while (cki != "Exit");
+            Console.ReadKey();
+        }
+        public static List<string> AddB(string book)
+        {
+            books.Add(book);
+            return books;
         }
     }
 }
